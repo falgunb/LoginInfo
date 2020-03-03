589099930cc350cdf6087949ee8dbafa822f7351
FalgunB <falgun.buddh@brillbrains.com>
2020-03-03 19:24:49 +0530

    LoginModel class

---
+++ app/src/main/java/com/k/logininfo/Model/Login.java
@@ -0,0 +1,41 @@
+package com.k.logininfo.Model;
+
+public class Login {
+
+    public String id;
+    public String userName;
+    public String password;
+
+    public Login() {
+    }
+
+    public Login(String userName, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
